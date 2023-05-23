057760d0a7b6049d6355fe9151314b48ee4d3b42
Pocehome <Pokehome@yandex.ru>
2023-05-23 23:44:12 +0300

    removed unnecessary

--- Figures_Pocehome/main.cpp
+++ Figures_Pocehome/main.cpp
@@ -8,44 +8,13 @@
 #include "Image.hpp"
 #include <iostream>
 
-void ShowFigures(Figure* figures[], size_t arr_szie) {
-	for (size_t i = 0; i < arr_szie; i++) {
-		std::cout << "Figure " << i << ":" << std::endl;
-		figures[i]->show();
-		std::cout << std::endl;
-	}
-}
-
 int main() {
 	Point point(1, 3);
-	/*std::cout << "Point:" << std::endl;
-	point.show();
-	std::cout << std::endl;*/
-
 	Line line(Point(1, 4), Point(5, 1));
-	/*std::cout << "Line:" << std::endl;
-	line.show();
-	std::cout << std::endl;*/
-
 	Circle circle(point, 5);
-	/*std::cout << "Circle:" << std::endl;
-	circle.show();
-	std::cout << std::endl;*/
-
 	Triangle triangle(Point(0, 0), Point(0, 3), Point(4, 0));
-	/*std::cout << "Triangle:" << std::endl;
-	triangle.show();
-	std::cout << std::endl;*/
-
 	Rectangle rectangle(Point(), 3, 5.6);
-	/*std::cout << "Rectangle:" << std::endl;
-	rectangle.show();
-	std::cout << std::endl;*/
-
 	Square square(Point(-3.44, -10), 5);
-	/*std::cout << "Square:" << std::endl;
-	square.show();
-	std::cout << std::endl;*/
 
 	Image im;
 	im.append(&point);
